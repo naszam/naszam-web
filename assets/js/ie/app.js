7204c5006c95ffca4a42e75c210663d4990b6363
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2019-09-28 01:14:39 +0100

    Update app.js

--- assets/js/ie/app.js
+++ assets/js/ie/app.js
@@ -1,4 +1,4 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/js/ie/', function() {
+particlesJS.load('particles-js', 'assets/js/ie/particlesjs-config.js', function() {
   console.log('callback - particles.js config loaded');
 });
