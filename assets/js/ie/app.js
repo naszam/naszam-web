6978ba3196f635152a99807dddf397d9da1fba95
Nazzareno Massari <nazzareno@nazzarenomassari.com>
2019-09-28 01:26:01 +0100

    Delete app.js

--- assets/js/ie/app.js
+++
@@ -1,4 +0,0 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/js/ie/particlesjs-config.js', function() {
-  console.log('callback - particles.js config loaded');
-});
